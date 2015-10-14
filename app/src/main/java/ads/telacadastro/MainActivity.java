a724e65c865bdadae33d6c24297cd4b8f5dd230d
albraga <alexandre_cgr@hotmail.com>
2015-10-14 10:36:22 -0300

    assinatura salvar

--- app/src/main/java/ads/telacadastro/MainActivity.java
+++ app/src/main/java/ads/telacadastro/MainActivity.java
@@ -2,6 +2,7 @@ package ads.telacadastro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
+    }
+
+    public void salvar(View view) {
+
     }
 }
